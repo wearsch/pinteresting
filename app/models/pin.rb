2178ffa02b4fa6170f7c9a574e2dbfd9e4970afc
Mark Wearsch <contact@wearsch.com>
2015-12-02 15:16:00 -0500

    Updated for AWS

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, 
-		:style => { :medium => "300x300>", :thumb => "100x100>" },
-		:storage => :s3,
-		:bucket => ENV['msw001']
+		:style => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,12 @@ Rails.application.configure do
   #  :access_key_id => ENV['AKIAIPZXCTHIV7UDNRTA'],
   #  :secret_access_key => ENV['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']
   #}
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
 }
 end
