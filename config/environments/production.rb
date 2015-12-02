1fbaa66092e1d849375168a142712a0bf205ad54
Mark Wearsch <contact@wearsch.com>
2015-12-01 19:32:31 -0500

    fixed error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
   :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['msw001'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AKIAIPZXCTHIV7UDNRTA'],
+    :secret_access_key => ENV['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']
   }
 }
 end
